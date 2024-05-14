e57548965f37814133643b4629270157745d60e3
Harold <rnarsonharold@gmail.com>
2024-05-15 01:07:53 +0200

    refactor: go to home screen from splash screen

--- lib/views/screens/splash/splash_store.dart
+++ lib/views/screens/splash/splash_store.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:song_lyrics/constants/helpers.dart';
-import 'package:song_lyrics/main.dart';
+import 'package:song_lyrics/views/screens/home/home_screen.dart';
 
 class SplashStore extends ChangeNotifier {
   void init(BuildContext context) {
     Timer(const Duration(seconds: 2), () {
-      Helpers.goTo(context, const MyHomePage(title: 'Flutter Demo Home Page'));
+      Helpers.goTo(context, const HomeScreen());
     });
   }
 }
